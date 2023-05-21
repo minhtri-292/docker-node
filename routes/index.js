68f040017e74bb9c1ce5b806a95218338c80fd20
minhtri-292 <minh.nc1229@gmail.com>
2023-05-21 18:49:47 +0700

    hello nodes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Docker NodeJS', name: 'James' });
+  res.render('index', { title: 'Docker NodeJS', name: 'James22' });
 });
 
 module.exports = router;
